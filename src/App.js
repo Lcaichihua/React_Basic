fcb02b30e702ea71edde1a1b482d57e284e927c1
Larry Caichihua Vergara <lcaichihua@gmail.com>
2019-05-01 22:52:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
+
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css' ;
+
+
+// function Hello(props){
+
+//   return <h2> {props.title} </h2>
+// }
+// const Hello =(props) => <h2>{props.title}</h2>
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello,World {this.props.name}</h1>;
+  }
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Hello title="xddddddd"/>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.cavetech.pe"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        
+          https://www.cavetech.pe/
         </a>
       </header>
     </div>
